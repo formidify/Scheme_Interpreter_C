a35466d6d565e3b2231307107e3fcbb510307530
tinaliu000 <tina.liu000@gmail.com>
2017-10-10 19:26:13 -0500

    Fix display()

--- linkedlist.c
+++ linkedlist.c
@@ -5,109 +5,98 @@
 #include <string.h>
 
 /*
- * Create an empty list (a new Value object of type NULL_TYPE).
- */
+* Create an empty list (a new Value object of type NULL_TYPE).
+*/
 Value *makeNull(){
-    Value *head = malloc(sizeof(Value));
-    if(!head){
-        printf("Out of memory");
-    }
-    head->type= NULL_TYPE;
-    return head;
+   Value *list = malloc(sizeof(Value));
+   if(!list){
+      printf("Out of memory");
+   }
+   list->type= NULL_TYPE;
+   return list;
 }
 
 /*
- * Create a nonempty list (a new Value object of type CONS_TYPE).
- */
+* Create a nonempty list (a new Value object of type CONS_TYPE).
+*/
 Value *cons(Value *car, Value *cdr) {
-    Value *list = malloc(sizeof(Value));
-    list->type = CONS_TYPE;
-    list->c.car = car;
-    list->c.cdr = cdr;
-    return list;
-}
-
-char* getSpecifier(Value *val){
-    switch (val->type) {
-        case INT_TYPE:
-            // do something with val.i
-            return "%i";
-        case DOUBLE_TYPE:
-            // do something with val.d
-            return "%d";
-        
-        /* ... */
-        default:
-            return "cool";
-              /* ??? */
-    }
+   Value *list = malloc(sizeof(Value));
+   list->type = CONS_TYPE;
+   list->c.car = car;
+   list->c.cdr = cdr;
+   return list;
 }
 
 /*
- * Print a representation of the contents of a linked list.
- */
+* Print a representation of the contents of a linked list.
+*/
 void display(Value *list){
-    if (list->type == NULL_TYPE) {
-        printf("bad");
-    }
-    else {
-        char *x = getSpecifier(list->c.car);
-        printf(getSpecifier(list->c.car), list->c.car);
-        printf("%i\n", *list->c.car);
-        //strcpy(a,  getSpecifier(list->c.car));
-        //strcpy(b, "%");
-        //strcat(a, b);
-//        printf("%s", strcat("%%", x));
-        
-    }
-    
+   if (list->type == NULL_TYPE) {
+      printf("\n");
+   }
+   else {
+      switch (list->c.car->type) {
+         case INT_TYPE:
+            printf("%i ", list->c.car->i);
+            break;
+         case DOUBLE_TYPE:
+            printf("%f ", list->c.car->d);
+            break;
+         case STR_TYPE:
+            printf("%s ", list->c.car->s);
+            break;
+         default:
+            printf("TODO");
+      }
+      display(list->c.cdr);
+   }
 }
 
 /*
- * Get the car value of a given list.
- * (Uses assertions to ensure that this is a legitimate operation.)
- */
+* Get the car value of a given list.
+* (Uses assertions to ensure that this is a legitimate operation.)
+*/
 Value *car(Value *list);
 
 /*
- * Get the cdr value of a given list.
- * (Uses assertions to ensure that this is a legitimate operation.)
- */
+* Get the cdr value of a given list.
+* (Uses assertions to ensure that this is a legitimate operation.)
+*/
 Value *cdr(Value *list);
 
 /*
- * Test if the given value is a NULL_TYPE value.
- * (Uses assertions to ensure that this is a legitimate operation.)
- */
+* Test if the given value is a NULL_TYPE value.
+* (Uses assertions to ensure that this is a legitimate operation.)
+*/
 bool isNull(Value *value);
 
 /*
- * Compute the length of the given list.
- * (Uses assertions to ensure that this is a legitimate operation.)
- */
+* Compute the length of the given list.
+* (Uses assertions to ensure that this is a legitimate operation.)
+*/
 int length(Value *value);
 
 /*
- * Create a new linked list whose entries correspond to the given list's
- * entries, but in reverse order.  The resulting list is a deep copy of the
- * original: that is, there should be no shared memory between the original
- * list and the new one.
- *
- * (Uses assertions to ensure that this is a legitimate operation.)
- *
- * FAQ: What if there are nested lists inside that list?
- * ANS: There won't be for this assignment. There will be later, but that will
- *      be after we've got an easier way of managing memory.
- */
+* Create a new linked list whose entries correspond to the given list's
+* entries, but in reverse order.  The resulting list is a deep copy of the
+* original: that is, there should be no shared memory between the original
+* list and the new one.
+*
+* (Uses assertions to ensure that this is a legitimate operation.)
+*
+* FAQ: What if there are nested lists inside that list?
+* ANS: There won't be for this assignment. There will be later, but that will
+*      be after we've got an easier way of managing memory.
+*/
 Value *reverse(Value *list);
 
 /*
- * Frees up all memory directly or indirectly referred to by list.
- *
- * (Uses assertions to ensure that this is a legitimate operation.)
- *
- * FAQ: What if there are nested lists inside that list?
- * ANS: There won't be for this assignment. There will be later, but that will
- *      be after we've got an easier way of managing memory.
+* Frees up all memory directly or indirectly referred to by list.
+*
+* (Uses assertions to ensure that this is a legitimate operation.)
+*
+* FAQ: What if there are nested lists inside that list?
+* ANS: There won't be for this assignment. There will be later, but that will
+*      be after we've got an easier way of managing memory.
 */
 void cleanup(Value *list);
