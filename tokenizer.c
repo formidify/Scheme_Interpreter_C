35279b72f3c98971c2d8c9b5da05c193ee47a29a
tinaliu000 <tina.liu000@gmail.com>
2017-10-22 22:42:03 -0500

    minor edit

--- tokenizer.c
+++ tokenizer.c
@@ -15,44 +15,44 @@
 
 bool isDigit(char c){
     return (c == '0' || c == '1' || c == '2' ||
-            c == '3' || c == '4' || c == '5' ||
-            c == '6' || c == '7' || c == '8' ||
-            c == '9');
+    c == '3' || c == '4' || c == '5' ||
+    c == '6' || c == '7' || c == '8' ||
+    c == '9');
 }
 
 bool isLetter(char c){
-	return (c == 'a' || c == 'i' || c == 'q' ||
-			c == 'b' || c == 'j' || c == 'r' ||
-			c == 'c' || c == 'k' || c == 's' ||
-			c == 'd' || c == 'l' || c == 't' ||
-			c == 'e' || c == 'm' || c == 'u' ||
-			c == 'f' || c == 'n' || c == 'v' ||
-			c == 'g' || c == 'o' || c == 'w' ||
-			c == 'h' || c == 'p' || c == 'x' ||
-			c == 'y' || c == 'z' || c == 'A' ||
-			c == 'B' || c == 'J' || c == 'R' ||
-			c == 'C' || c == 'K' || c == 'S' ||
-			c == 'D' || c == 'L' || c == 'T' ||
-			c == 'E' || c == 'M' || c == 'U' ||
-			c == 'F' || c == 'N' || c == 'V' ||
-			c == 'G' || c == 'O' || c == 'W' ||
-			c == 'H' || c == 'P' || c == 'X' ||
-			c == 'I' || c == 'Q' || c == 'Y' ||
-			c == 'Z');
+    return (c == 'a' || c == 'i' || c == 'q' ||
+    c == 'b' || c == 'j' || c == 'r' ||
+    c == 'c' || c == 'k' || c == 's' ||
+    c == 'd' || c == 'l' || c == 't' ||
+    c == 'e' || c == 'm' || c == 'u' ||
+    c == 'f' || c == 'n' || c == 'v' ||
+    c == 'g' || c == 'o' || c == 'w' ||
+    c == 'h' || c == 'p' || c == 'x' ||
+    c == 'y' || c == 'z' || c == 'A' ||
+    c == 'B' || c == 'J' || c == 'R' ||
+    c == 'C' || c == 'K' || c == 'S' ||
+    c == 'D' || c == 'L' || c == 'T' ||
+    c == 'E' || c == 'M' || c == 'U' ||
+    c == 'F' || c == 'N' || c == 'V' ||
+    c == 'G' || c == 'O' || c == 'W' ||
+    c == 'H' || c == 'P' || c == 'X' ||
+    c == 'I' || c == 'Q' || c == 'Y' ||
+    c == 'Z');
 }
 
 bool isInitial(char c){
-	return (isLetter(c) || c == '!' || c == '$' ||
-			c == '%' || c == '&' || c == '*' ||
-			c == '/' || c == ':' || c == '<' ||
-			c == '=' || c == '>' || c == '?' ||
-			c == '~' || c == '_' || c == '^');
+    return (isLetter(c) || c == '!' || c == '$' ||
+    c == '%' || c == '&' || c == '*' ||
+    c == '/' || c == ':' || c == '<' ||
+    c == '=' || c == '>' || c == '?' ||
+    c == '~' || c == '_' || c == '^');
 }
 
 
 bool isSubsequent(char c){
-	return (isInitial(c) || isDigit(c) || c == '.' ||
-			c == '+' || c == '-');
+    return (isInitial(c) || isDigit(c) || c == '.' ||
+    c == '+' || c == '-');
 }
 
 Value *tokenizeNumber(char charRead, bool hasSign, bool hasDot){
@@ -77,18 +77,18 @@ Value *tokenizeNumber(char charRead, bool hasSign, bool hasDot){
     }
 
     while(charRead != EOF && charRead != '(' &&
-        charRead != ')' && charRead != ' '){
-            if(isDigit(charRead)){
-                add(number, number->size - 1, charRead);
-                charRead = fgetc(stdin);
-            } else if(charRead == '.' && !hasDot){
-                add(number, number->size - 1, charRead);
-                hasDot = true;
-                charRead = fgetc(stdin);
-            }else {
-                printf("Tokenization error, unrecognized symbol in number.\n");
-                texit(0);
-            }
+    charRead != ')' && charRead != ' '){
+        if(isDigit(charRead)){
+            add(number, number->size - 1, charRead);
+            charRead = fgetc(stdin);
+        } else if(charRead == '.' && !hasDot){
+            add(number, number->size - 1, charRead);
+            hasDot = true;
+            charRead = fgetc(stdin);
+        }else {
+            printf("Tokenization error, unrecognized symbol in number.\n");
+            texit(0);
+        }
     }
 
     ungetc(charRead, stdin);
@@ -111,14 +111,14 @@ Value *tokenizeSymbol(char charRead){
     add(symbol, 1, '\0');
     charRead = fgetc(stdin);
     while(charRead != EOF && charRead != '(' &&
-        charRead != ')' && charRead != ' '){
-            if(isSubsequent(charRead)){
-                add(symbol, symbol->size - 1, charRead);
-                charRead = fgetc(stdin);
-            } else {
-                printf("Tokenization error, invalid symbol.\n");
-                texit(0);
-            }
+    charRead != ')' && charRead != ' '){
+        if(isSubsequent(charRead)){
+            add(symbol, symbol->size - 1, charRead);
+            charRead = fgetc(stdin);
+        } else {
+            printf("Tokenization error, invalid symbol.\n");
+            texit(0);
+        }
     }
     ungetc(charRead, stdin);
     Value *symbolType = makeNull();
@@ -130,7 +130,7 @@ Value *tokenizeSymbol(char charRead){
 Value *tokenize(){
     char charRead;
     Value *list = makeNull();
-	bool init = false;
+    bool init = false;
     charRead = fgetc(stdin);
     while (charRead != EOF){
         if (charRead == '('){
@@ -147,7 +147,7 @@ Value *tokenize(){
             if (charRead == 't' || charRead == 'f') {
                 charRead = fgetc(stdin);
                 if (charRead == '(' || charRead == ')' ||
-                    charRead == ' ') {
+                charRead == ' ') {
                     charRead = ungetc(charRead, stdin);
                     Value *boolType = makeNull();
                     boolType->type = BOOL_TYPE;
@@ -158,7 +158,7 @@ Value *tokenize(){
                     }
                     list = cons(boolType, list);
                 } else {
-                   printf("Tokenization error: # is not a valid symbol.\n");
+                    printf("Tokenization error: # is not a valid symbol.\n");
                     texit(0);
                 }
             } else {
@@ -196,34 +196,34 @@ Value *tokenize(){
         } else if(isInitial(charRead)){
             Value *symbolType = tokenizeSymbol(charRead);
             list = cons(symbolType, list);
-	//changes made by Chae. No other changes except bool init = false;
-	// in beginning of function
-        } else if(charRead == '"' && init == false){
-			init = true;
-			//char *string;
-			int acc = 1;
-			long int cur = ftell(stdin);
-			charRead = fgetc(stdin);
-			while (charRead != EOF || (charRead != '"' && init == true)){
-				acc += 1;
-				charRead = fgetc(stdin);
-			}
-			if (charRead == EOF){
-				printf("Err: Quotations do not pair up");
-				texit(0);
-			}else if(charRead == '"' && init == true){
-				fseek(stdin, cur, 0);
-				char *string[acc + 2];
-				for (int i = 0; i < acc + 1; i++){
-					string[i] = fgetc(stdin);
-				}
-				string[i+1] = '\0';
-				init = false;
-				Value *strType = makeNull();
-				strType->type = STR_TYPE;
-				strType->s = string;
-				list = cons(strType, list);
-			}
+            //changes made by Chae. No other changes except bool init = false;
+            // in beginning of function
+        } else if(charRead == '\"' && init == false){
+            init = true;
+            //char *string;
+            int acc = 1;
+            long int cur = ftell(stdin);
+            charRead = fgetc(stdin);
+            while (charRead != EOF || (charRead != '"' && init == true)){
+                acc += 1;
+                charRead = fgetc(stdin);
+            }
+            if (charRead == EOF){
+                printf("Err: Quotations do not pair up");
+                texit(0);
+            }else if(charRead == '"' && init == true){
+                fseek(stdin, cur, 0);
+                char *string[acc + 2];
+                for (int i = 0; i < acc + 1; i++){
+                    string[i] = fgetc(stdin);
+                }
+                string[i+1] = '\0';
+                init = false;
+                Value *strType = makeNull();
+                strType->type = STR_TYPE;
+                strType->s = string;
+                list = cons(strType, list);
+            }
         }
         charRead = fgetc(stdin);
     }
@@ -237,7 +237,7 @@ Value *tokenize(){
 void displayTokens(Value *list){
     assert(list != NULL);
     switch(list->type) {
-        case OPEN_TYPE:
+            case OPEN_TYPE:
             printf("%s:open", "(");
             break;
         case CLOSE_TYPE:
